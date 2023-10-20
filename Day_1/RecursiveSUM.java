06887d63061102ad644ebe5ae581e579d6f3d585
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-10-20 19:08:41 +0545

    initial commit

---
+++ Day_1/RecursiveSUM.java
@@ -0,0 +1,11 @@
+class RecursiveSUM{
+    static int show(int n){
+        if(n==4)
+        return n;
+        else
+        return 2*show(n+1);
+    }
+    public static void main(String args[]){
+        System.out.println(show(2));
+    }
+}
