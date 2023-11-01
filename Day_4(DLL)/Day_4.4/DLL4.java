e4bd30e67496b5d0842f39e6d7ea1de9bb3beaa3
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-11-01 09:52:38 +0545

    Day 4 commit

--- Day_4(DLL)/Day_4.4/DLL4.java
+++ Day_4(DLL)/Day_4.4/DLL4.java
@@ -13,37 +13,40 @@
     }
         void insert(int new_data){
             Node new_node=new Node(new_data);
-            //new_node.prev=null;
-            //new_node.next=null;
-            if(head!=null){
+            new_node.prev=null;
+            new_node.next=head;
+            
+                    if(head!=null){
                head.prev=new_node;
             }
             head=new_node;
         }
-        void display(Node n){
-            // Node n=head;
-            Node p=null;
-            System.out.println("Forward direction");
-            while(n!=null){
-                System.out.print(n.data + " ---> ");
+        
+        void display(Node n)
+        {
+            Node p = null;
+            System.out.println("Forward direction:");
+            while(n != null)
+            {
+                System.out.println(n.data+"---> ");
                 p=n;
                 n=n.next;
             }
-        
-            System.out.println("Backward direction");
-            while(p!=null){
-                System.out.print(p.data + " ---> ");
+            System.out.println("----------------------------");
+            System.out.println("Backward direction:");
+            while(p != null)
+            {
+                System.out.println(p.data+"---> ");
                 p=p.prev;
             }
         }
-    
     public static void main(String args[]){
         DLL4 L1=new DLL4();
         L1.head=new Node(50);
         L1.insert(40);
         L1.insert(30);
-        L1.insert(20);
-        L1.insert(10);
+         L1.insert(20);
+        // L1.insert(10);
         L1.display(L1.head);
     }
 
